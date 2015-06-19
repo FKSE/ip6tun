4ffed12895b338c6c5805f56adc9b19c38e1695c
Frido Koch <info@fridokoch.de>
2015-06-19 17:09:52 +0200

    Removed Key field

--- protocol/message.go
+++ protocol/message.go
@@ -10,19 +10,18 @@ const (
 	TypeUpdate      = 0x01
 	TypeDelete      = 0x02
 	TypeAcknowledge = 0x03
-	TypeError = 0x04
+	TypeErrorNoTunnel = 0x04 // Unable to create tunnel
 )
 
 type Message struct {
 	Type       byte
 	ClientId   [32]byte
-	Key        [14]byte
 	LocalPort  uint16
 	RemotePort uint16
 }
 
 func (m *Message) String() string {
-	return fmt.Sprintf("Type: %x, Sender-Id: % x, Key: % x, Local-Port: %d, Remote-Port: %d", m.Type, m.ClientId, m.Key, m.LocalPort, m.RemotePort)
+	return fmt.Sprintf("Type: %x, Sender-Id: % x, Local-Port: %d, Remote-Port: %d", m.Type, m.ClientId, m.LocalPort, m.RemotePort)
 }
 
 func (m *Message) Marshal() ([]byte, error) {
